ab564f6bdefa8b655f0f0ca61f0a1de96e192e40
lalet <laletscaria@yahoo.co.in>
2017-10-05 17:57:53 -0400

    Added code to determine winner

--- src/index.js
+++ src/index.js
@@ -40,6 +40,9 @@ class Board extends React.Component {
  //Function to handle click
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O' ;
     this.setState({
 		   squares: squares,
@@ -54,8 +57,14 @@ class Board extends React.Component {
   
   //Basic page rendering
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    
     return (
       <div>
         <div className="status">{status}</div>
@@ -93,6 +102,27 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+//Winning helper function
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================

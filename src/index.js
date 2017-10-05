d6b93131c82d7209ad435b4c78966f1cf580ab1a
lalet <laletscaria@yahoo.co.in>
2017-10-05 17:04:39 -0400

    Setting the current state

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 
 
 class Square extends React.Component {
-  render() {
+   constructor(){
+     super();
+     this.state = {
+       value : null
+     }
+   }
+   render() {
     return (
-      <button className="square" onClick={() => alert('click')}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
